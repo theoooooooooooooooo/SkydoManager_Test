2bcea9e15491a31e7a2dd96b0c6206909e3cc997
Théo <theo@gmail.com>
2023-07-05 08:04:01 +0200

    Ajout menu page form

--- src/Controller/DevisController.php
+++ src/Controller/DevisController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 use App\Entity\Devis;
 use App\Form\DevisFormType;
 use App\Repository\DevisRepository;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request; 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
