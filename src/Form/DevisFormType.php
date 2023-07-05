4384c13fa0f6a180b18e6830be7367c44bcca6ed
Théo <theo@gmail.com>
2023-07-05 11:23:08 +0200

    Style

--- src/Form/DevisFormType.php
+++ src/Form/DevisFormType.php
@@ -15,34 +15,58 @@ class DevisFormType extends AbstractType
         ->add('siteEcom', CheckboxType::class, [
             'label' => 'Site Ecommerce',
             'required' => false,
+            'attr' => [
+                'class' => 'custom-label-checkbox'
+            ]
         ])
         ->add('siret', CheckboxType::class, [
             'label' => 'Site Vitrine',
             'required' => false,
+            'attr' => [
+                'class' => 'custom-label-checkbox'
+            ]
         ])
         ->add('siteCustom', CheckboxType::class, [
             'label' => 'Site Custom',
             'required' => false,
+            'attr' => [
+                'class' => 'custom-label-checkbox'
+            ]
         ])
         ->add('maintenance', CheckboxType::class, [
             'label' => 'Maintenance & Sécurisation',
             'required' => false,
+            'attr' => [
+                'class' => 'custom-label-checkbox'
+            ]
         ])
         ->add('logo', CheckboxType::class, [
             'label' => 'Création de logo',
             'required' => false,
+            'attr' => [
+                'class' => 'custom-label-checkbox'
+            ]
         ])
         ->add('identiteVisuelle', CheckboxType::class, [
             'label' => 'Identité Visuelle',
             'required' => false,
+            'attr' => [
+                'class' => 'custom-label-checkbox'
+            ]
         ])
         ->add('print', CheckboxType::class, [
             'label' => 'Flyer - 4/3 - Affiches',
             'required' => false,
+            'attr' => [
+                'class' => 'custom-label-checkbox'
+            ]
         ])
         ->add('shooting', CheckboxType::class, [
             'label' => 'Shooting Photo',
             'required' => false,
+            'attr' => [
+                'class' => 'custom-label-checkbox'
+            ]
         ]);
     }
 
